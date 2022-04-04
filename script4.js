148fb97798866b1c84a636379acdd51ab95d119c
MichaelCostanzaJr <77464599+MichaelCostanzaJr@users.noreply.github.com>
2022-04-04 15:43:56 -0700

    103_Hw_1

---
+++ script4.js
@@ -0,0 +1,48 @@
+let userFirstName="Michael";
+let userLastName="Costanza";
+let userEmail="Costanzamike96@gmail.com";
+let userDateOfMonth="May";
+let userDateOfDay=18;
+let userDateOfYear=1996;
+let userPhoneNumber= "760-390-9563";
+let userPassword= "Sandiegoglobalknowledgeuniversity123!";
+let howDidYouHearAboutUs="Online Promotion";
+let userStreetAddress= "3999 Henry Ct";
+let userApptNumber= "Not Applicable";
+let userCity="Oceanside";
+let userState="California";
+let userZipcode="92058";
+let userCountry="United States"
+let userBillingShippingSame="Yes";
+let userBillingAddress="3999 Henry Ct Oceanside,Ca 92058";
+let userShippingAddress="3999 Henry Ct Oceanside,Ca 92058";
+let movieTitle="Batman";
+let movieGenre="Action-Suspence";
+let movieRating="Rated R"
+let gameTitle="Call of Duty";
+let gameGenre="Action";
+let gameRating="M For Mature";
+
+document.write(`<h1> First Name: ${userFirstName}</h1>`)
+document.write(`<h1> Last Name: ${userLastName}</h1>`)
+document.write(`<h1> Email: ${userEmail}</h1>`)
+document.write(`<h1> Month Born: ${userDateOfMonth}</h1>`)
+document.write(`<h1> Day Born : ${userDateOfDay}</h1>`)
+document.write(`<h1> Year Born: ${userDateOfYear}</h1>`)
+document.write(`<h1> Phone Number: ${userPhoneNumber}</h1>`)
+document.write(`<h1> Password: ${userPassword}</h1>`)
+document.write(`<h1> How Did You Hear About Us?: ${howDidYouHearAboutUs}</h1>`)
+document.write(`<h1> Street Adress: ${userStreetAddress}</h1>`)
+document.write(`<h1> APT Number: ${userApptNumber}</h1>`)
+document.write(`<h1> City: ${userCity}</h1>`)
+document.write(`<h1> State: ${userState}</h1>`)
+document.write(`<h1> Country: ${userCountry}</h1>`)
+document.write(`<h1> Is your Billing and Shipping address the same? ${userBillingShippingSame}</h1>`)
+document.write(`<h1> Billing Address: ${userBillingAddress}</h1>`)
+document.write(`<h1> Shipping Address: ${userShippingAddress}</h1>`)
+document.write(`<h1> Movie Title: ${movieTitle}</h1>`)
+document.write(`<h1> Movie Genre: ${movieGenre}</h1>`)
+document.write(`<h1> Movie Rating: ${movieRating}</h1>`)
+document.write(`<h1> Game Title: ${gameTitle}</h1>`)
+document.write(`<h1> Game Genre: ${gameGenre}</h1>`)
+document.write(`<h1> Game Rating: ${gameRating}</h1>`)
